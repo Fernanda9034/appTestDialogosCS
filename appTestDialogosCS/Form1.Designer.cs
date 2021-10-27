c82d6bc200c644fbb0a2c1a5d9c13320fd5f726a
Fernanda9034 <i21050345@monclova.tecnm.mx>
2021-10-26 21:17:22 -0500

    Add project files.

---
+++ appTestDialogosCS/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace appTestDialogosCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnAbrir.Location = new System.Drawing.Point(122, 43);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(114, 51);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnGuardar.Location = new System.Drawing.Point(330, 43);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(103, 51);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ appTestDialogosCS/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace appTestDialogosCS
+{
+    public partial class Form1 : Form
+    {
+        private string linea;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+           
+            OpenFileDialog dialogo = new OpenFileDialog();
+            if(dialogo.ShowDialog() == DialogResult.OK);
+            {
+                MessageBox.Show(dialogo.FileName);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogo = new SaveFileDialog();
+            if (dialogo.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(dialogo.FileName);
+            }
+
+        }
+     
+        //int contador = 0;
+        //string linea = "";
+
+        // lee el archivo te texto linea por linea.
+        //StreamReader file = new StreamReader(@"C:\test.txt");
+
+        //while ((linea = file.ReadLine()) != null)
+        //{
+        //Console.WriteLine(linea);
+        //contador++;
+        //}
+        //file.Close();
+        //Console.WriteLine("There were {0} lines.", contador);
+        //Suspend the screen.  
+        //Console.ReadLine();
+        //}
+        
+    }
+}
